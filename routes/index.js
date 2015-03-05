b39b74ff7de8c33d82e843a21218f0eaf02a581e
Peter deHaan <peter@deseloper.com>
2015-03-05 10:18:20 -0800

    adding verifier

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var verify = require('browserid-verify')();
+
 var router = express.Router();
 
 /* GET home page. */
@@ -11,6 +13,23 @@ router.get('/images', function(req, res, next) {
 });
 
 router.post('/auth/login', function(req, res, next) {
+  console.log('assertion: %s', req.body.assertion);
+  verify(req.body.assertion, 'localhost:5000', function(err, email, response) {
+    if (err) {
+      // make sure no session is created
+      return console.log('There was an error : ' + err);
+    }
+
+    // If email is set, then the assertion was ok and response contains the full response.
+    // Set up your session and cookie as normal.
+
+    // If email is not set, then the request was fine but the assertion didn't check out.
+    // Do not set up the session and cookie. Instead provide a message to your users.
+
+    console.log('The asserted email address is : ' + email);
+    console.log('The entire reponse is :', response);
+  });
+
   res.render('auth/login', { title: 'Login' });
 });
 
